f1a8e07e30d8db2d1d08682574b57f712e4a7cb8
daluj <63384857+daluj@users.noreply.github.com>
2020-09-29 10:49:27 +0200

    Lesson3 missing element

---
+++ src/main/java/lesson3/PermMissingElem.java
@@ -0,0 +1,28 @@
+package lesson3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PermMissingElem {
+    public int solution(int[] A) {
+        int missing_element = 0;
+        int N = A.length;
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        
+        for (int i = 1; i <= N + 1; i++) {
+            map.put(i, 1);
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(map.containsKey(A[i])) {
+                map.remove(A[i]);
+            }
+        }
+
+        for (int key : map.keySet()) {
+            missing_element = key;
+        }
+
+        return missing_element;
+    }
+}
