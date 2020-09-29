7a54c7a056fc2a8d4d8188d9aef4cc696a19a00c
daluj <63384857+daluj@users.noreply.github.com>
2020-09-29 10:33:18 +0200

    Lesson 3 frog jump

--- src/main/java/lesson3/FrogJmp.java
+++ src/main/java/lesson3/FrogJmp.java
@@ -1,5 +1,12 @@
 package lesson3;
 
 public class FrogJmp {
-    
+    public int solution(int X, int Y, int D) {
+        int jumps;
+
+        double diff = Y - X;
+        jumps = (int) Math.ceil( diff / D );
+
+        return jumps;
+    }
 }
