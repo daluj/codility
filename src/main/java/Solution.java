e0094b5608a8591c16ce37c13f149a54e93b2227
daluj <63384857+daluj@users.noreply.github.com>
2020-10-15 11:25:04 +0200

    Add random generation in Solution templates

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -2,5 +2,8 @@ public class Solution {
 	public int solution(int input) {
 		return 1;
 	}
-    
+
+	public int solutionArray(int[] input) {
+		return 1;
+	}
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,5 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Random;
+
 import org.junit.jupiter.api.Test;
 
 public class SolutionTest {
@@ -8,9 +10,25 @@ public class SolutionTest {
         // Input
         Solution s = new Solution();
         int input = 1;
-
+        
         // Output
         int actual = s.solution(input);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void exampleRandomTest(){
+        // Input
+        Solution s = new Solution();
+
+        int N = 100;
+        int origin = 0;
+        int bound = 100;
+        int[] input = new Random().ints(N,origin,bound).toArray();
+
+        // Output
+        int actual = s.solutionArray(input);
         int expected = 1;
         assertEquals(expected, actual);
     }
